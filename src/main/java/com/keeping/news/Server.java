49ac66b3474d272fe23bb035425269b2499ccdb0
xufh <xufh@ifeng.com>
2018-04-08 13:18:55 +0800

    [添加] 添加项目后端环境搭建

---
+++ src/main/java/com/keeping/news/Server.java
@@ -0,0 +1,14 @@
+package com.keeping.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by keepspy on 2018/4/8.
+ */
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class,args);
+    }
+}
